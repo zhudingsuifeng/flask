f302eb32e3e17a289c396a22b3123c819183a5d5
fly <1002557401@qq.com>
2018-12-29 18:28:48 +0800

    datetime

--- hello.py
+++ hello.py
@@ -3,16 +3,20 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+from datetime import datetime
 
 app = Flask(__name__)   # 创建Flask类的对象，即程序实例
 # app.run(debug=True)
 
 Bootstrap(app)
+Moment(app)
 manager = Manager(app)  # 添加命令行解析器
 
 @app.route('/')  # app.route修饰器，把修饰的函数注册为路由，访问对应URL时的相应函数
 def index():
-    return render_template('index.html')
+    return render_template('index.html',current_time=datetime.utcnow())
     # render_template函数第一个参数是模板的文件名。随后的参数是键值对，表示模板中变量对应的真实值。
 
 @app.route('/user/<name>')
