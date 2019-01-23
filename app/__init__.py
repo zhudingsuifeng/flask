aefdd7cac10ad8e302bdc075aa815e89e68abac2
fly <1002557401@qq.com>
2019-01-23 14:11:23 +0800

    项目init文件，用于生成app对象，并把db关联到app对象

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+# coding = utf-8
+
+__author__ = 'fly'
+
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+
+from config import config
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    return app
