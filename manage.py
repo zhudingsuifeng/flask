efedc7248d47b2438b2578fbdf78f17c52b3b068
fly <1002557401@qq.com>
2019-01-24 16:13:14 +0800

    入口程序

--- manage.py
+++ manage.py
@@ -1,117 +1,22 @@
+# coding = utf-8
+
+import os
 import os, datetime, json, csv
-from flask import Flask, render_template, session, url_for, request, jsonify, Response
-# jsonify 用来生成json格式数据， Response 用于自定义响应形式, request 用于获取url参数数据
-from flask_script import Manager
+from flask import render_template, session, url_for, request, jsonify, Response
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
 
-from flask import make_response   # 自定义响应
-import mimetypes   # 用于设置响应headers['Content-Type']
-import tablib      # 用于导出各种格式的文件
-from sqlalchemy import func   # 用于func.count()统计行数
+from app import create_app
+from app.models import PosTransmst
+from config import config
 
-# 导入自定义orm文件
-from models.pos_transmst import db, PosTransmst
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-app = Flask(__name__)   # 创建Flask类的对象，即程序实例
-app.config['SECRET_KEY'] = 'flaskOracle'   # app.config字典用来存储框架，扩展和程序本身的配置变量。
-# SECRET_KEY 配置变量通用密钥。
-app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle+cx_oracle://HEX_SPCC:HEX_SPCC@frps.hexcloud.cn:31733/HEXDB'
-# oracle数据库连接字符串
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
-
-db.init_app(app)   # 将db对象
-Bootstrap(app)     # 模板
-manager = Manager(app)  # 添加命令行解析器
+app = create_app(os.getenv('development') or 'development')
+manager = Manager(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # return request.data
     return render_template('index.html')
-
-# 前端请求URL为：127.0.0.1:5000/search?deptcd=123&bizdt=123
-# 给出门店的号码(DEPTCD)和时间(BIZDT)返回门店对应时间交易量
-@app.route('/search')
-def search():
-    info = {"DEPTCD" : request.args.get('deptcd'),
-        "BIZDT" : request.args.get('bizdt')}
-    sale = PosTransmst.query.filter_by(
-        deptcd = info["DEPTCD"],
-        bizdt = datetime.datetime.strptime(info["BIZDT"], "%Y-%m-%d %H:%M:%S"),
-        saletp = 'SALE'
-        ).count()
-
-        # 精准查询 filter_by()
-    res = {"DEPTCD": info["DEPTCD"], "BIZDT": info["BIZDT"], "SALE": sale}
-    return jsonify(res)  # 向前端返回json格式数据
-
-# 前端请求URL：127.0.0.1:5000/query?bizdt=2017-12-31 00:00:00&flag=json
-# 127.0.0.1:5000/query?bizdt=2017-12-31 00:00:00&flag=csv
-# 给出时间(BIZDT)返回所有门店对应时间交易量列表
-@app.route('/query')
-def query():
-    # request.args.get() 以获取字典值的形式获取URL参数
-    info = {"BIZDT" : request.args.get('bizdt'),
-            "FLAG" : request.args.get('flag')}
-    # PosTransmst 在pos_transmst.py文件中定义的PosTransmst类,对应数据库中的POSTRANSMST表
-    # 按门店号分组group by，并统计每个门店指定日期的交易量
-    result = db.session.query(PosTransmst.deptcd, func.count()).filter_by(
-        bizdt = datetime.datetime.strptime(info["BIZDT"], "%Y-%m-%d %H:%M:%S"),
-        saletp = 'SALE'
-        ).group_by(PosTransmst.deptcd).all()
-    # datetime.datetime.strptime(res["BIZDT"], "%Y-%m-%d %H:%M:%S")  
-    # 将字符串转换为数据库可以识别的datetime格式
-
-    '''
-    result = db.session.query(PosTransmst.deptcd, func.count()).filter_by(   
-        bizdt = datetime.datetime.strptime(info["BIZDT"], "%Y-%m-%d %H:%M:%S"),
-        saletp = 'SALE'
-        ).group_by(PosTransmst.deptcd).limit(10).all()
-    # session.query() 要查询的列  select 
-    # func.count()   统计结果行数 count
-    # filter_by()    过滤条件   where
-    # group_by()     分组      group by
-    # limit()      限制显示结果行数  limit
-    # all()        最终结果
-    '''
-
-    # 将查询结果包装成想要的格式， 使用yield 应该能提高些效率
-    # 将[(1,2),(1,2)...] 格式转换为 [[0,1,2],[0,1,2]...]
-    def convert():
-        for dept, sale in result:
-            yield [dept, info["BIZDT"], sale]
-            # yield {"DEPTCD": dept, "BIZDT": info["BIZDT"], "SALE": sale} 
-
-    # 将字符格式的数据转换成json格式
-    def str2json():
-        for dept, sale in result:
-            yield {"DEPTCD": dept, "BIZDT": info["BIZDT"], "SALE": sale}
-
-    res = convert()   # res 数据list,tablib 导入的格式
-    data = tablib.Dataset(*res, headers = ["DEPTCD", "BIZDT", "SALE"])
-    # *res 数据， headers 表头
-
-    if info['FLAG'] == 'json':
-        return jsonify(list(str2json()))
-
-    elif info['FLAG'] == 'csv_file':
-        resp = make_response(data.csv)
-        filename = ''.join([info["BIZDT"], '.csv'])
-        mime_type = mimetypes.guess_type(filename)[0]
-        resp.headers['Content-Type'] = mime_type
-        resp.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
-        return resp
-        
-    elif info['FLAG'] == 'json_file':
-        resp = make_response(data.json)                    # 创建相应对象
-        filename = ''.join([info["BIZDT"], '.json'])       # 响应下载的文件名
-        mime_type = mimetypes.guess_type(filename)[0]      # 响应类型
-        resp.headers['Content-Type'] = mime_type           # 设置响应头
-        resp.headers['Content-Disposition'] = 'attachment; filename={}'.format(filename)
-        return resp
 
 @app.errorhandler(404)
 def page_not_found(e):
--- test.py
+++ test.py
@@ -1,75 +1,31 @@
-import os, datetime
-from flask import Flask, session, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+# coding = utf-8
 
-app = Flask(__name__)   # 创建Flask类的对象，即程序实例
-# app.config['SECRET_KEY'] = 'flaskOracle'   # app.config字典用来存储框架，扩展和程序本身的配置变量。
-app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle+cx_oracle://HEX_SPCC:HEX_SPCC@frps.hexcloud.cn:31733/HEXDB'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SQLALCHEMY_ECHO'] = True
+import os
+import os, datetime, json, csv
+from flask import render_template, session, url_for, request, jsonify, Response
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
 
-from sqlalchemy import Column, DateTime, Index, Integer, Numeric, String, func
-from sqlalchemy.schema import FetchedValue
-from flask_sqlalchemy import SQLAlchemy
+from app import create_app
+from app.models import PosTransmst
+from config import config
 
-db = SQLAlchemy(app)   # 创建SQLAlchemy对象
+app = create_app(os.getenv('development') or 'development')
 
-class PosTransmst(db.Model):
-    __tablename__ = 'pos_transmst'
-    __table_args__ = (
-        db.Index('idx_pos_transmst_bizdt_dept', 'bizdt', 'deptcd'),
-    )
+manager = Manager(app)
 
-    transno = db.Column(db.Numeric(20, 0, asdecimal=False), primary_key=True)
-    bizdt = db.Column(db.DateTime, index=True)
-    ctrno = db.Column(db.Numeric(20, 0, asdecimal=False), index=True)
-    tillcd = db.Column(db.String(20))
-    saleid = db.Column(db.Integer)
-    transtp = db.Column(db.String(1))
-    transrsn = db.Column(db.String(10))
-    saletp = db.Column(db.String(10))
-    deptcd = db.Column(db.String(10))
-    opercd = db.Column(db.String(10))
-    membercd = db.Column(db.String(20))
-    grossamt = db.Column(db.Numeric(19, 4), server_default=db.FetchedValue())
-    netamt = db.Column(db.Numeric(19, 4))
-    discamt = db.Column(db.Numeric(19, 4))
-    curqty = db.Column(db.Numeric(19, 4), server_default=db.FetchedValue())
-    ctmcount = db.Column(db.Integer, server_default=db.FetchedValue())
-    ctmtp = db.Column(db.String(10))
-    tablecd = db.Column(db.String(20))
-    tradedt = db.Column(db.DateTime)
-    procdt = db.Column(db.DateTime)
-    salesmethod = db.Column(db.Integer)
-    orddt = db.Column(db.DateTime)
-    orderby = db.Column(db.String(20))
-    point = db.Column(db.Numeric(19, 4))
-    tableregion = db.Column(db.String(50))
-    upddt = db.Column(db.DateTime)
-    memberid = db.Column(db.Numeric(20, 0, asdecimal=False))
-    emccd = db.Column(db.String(10))
-    cvno = db.Column(db.String(30))
-    emcd = db.Column(db.String(20))
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # return request.data
+    return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-info = {"DEPTCD" : '131101',
-        "BIZDT" : '2017-12-31 00:00:00'}
-        
-sale = db.session.query(func.count(PosTransmst.saletp)).filter_by(
-            deptcd = info["DEPTCD"],
-            bizdt = datetime.datetime.strptime(info["BIZDT"], "%Y-%m-%d %H:%M:%S"),
-            saletp = 'SALE'
-        ).all()
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
-info["SALE"] = sale
-
-print(jsonify(info))
-
-result = [i for i in range(20)]
-
-def z():
-    for dept in result:
-        yield {"DEPTCD": dept, "BIZDT": "2017-12-31 00:00:00", "SALE": 10} 
-
-print(list(z()))
-# print("hello")
+if __name__ == "__main__":
+    manager.run()
